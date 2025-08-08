e313735edb76e05bb5219f62fb6dfa823ad225e6
Dev Jivani <djjivanidjpatel@gmail.com>
2025-08-08 11:16:19 +0530

    string

---
+++ 01_basics/05_string.js
@@ -0,0 +1,36 @@
+const name = "hitesh"
+const repoCount = 50
+
+// console.log(name + repoCount + " Value");
+
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('hitesh-hc-com')
+
+
+console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+
+// // console.log(gameName.length);
+// // console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('t'));
+
+// const newString = gameName.substring(0, 4)
+// console.log(newString);   // hite
+
+const anotherString = gameName.slice(-2, 4)
+console.log(anotherString);
+
+// const newStringOne = "   hitesh    "
+// console.log(newStringOne);   // "   hitesh    "
+// console.log(newStringOne.trim());  // hitesh
+
+// const url = "https://hitesh.com/hitesh%20choudhary"
+
+// console.log(url.replace('%20', '-'))
+
+// console.log(url.includes('sundar'))   //  check word is available or not
+
+// console.log(gameName.split('-'));  
