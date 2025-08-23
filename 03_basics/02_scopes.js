b1e0de421919536239c866ad8fa956c4228218c2
Dev Jivani <djjivanidjpatel@gmail.com>
2025-08-23 20:00:16 +0530

    scope

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,15 @@
+//var c = 300
+// let a = 300
+if (true) {
+    let a = 10        //  when let and const declare in scope and access outside of the scope that time error occured in let and const but not in var
+    const b = 20
+    var c = 30
+    // console.log("INNER: ", a);
+    
+}
+
+
+
+console.log(a);
+console.log(b);
+console.log(c);
