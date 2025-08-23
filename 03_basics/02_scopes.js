a1b1ff96f28973155bba6606c8e8c45fb8dedbdb
Dev Jivani <djjivanidjpatel@gmail.com>
2025-08-23 20:19:47 +0530

    scope level and mini hoisting

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -10,6 +10,58 @@ if (true) {
 
 
 
-console.log(a);
-console.log(b);
-console.log(c);
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+
+
+
+function one(){
+    const username = "hitesh"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+     two()
+
+}
+
+// one()
+
+if (true) {
+    const username = "hitesh"
+    if (username === "hitesh") {
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+
+// ++++++++++++++++++ interesting ++++++++++++++++++
+
+
+// here two way to declare a function
+
+// way 1 :  in this we can access function before the function initializition
+
+console.log(addone(5))        
+
+function addone(num){
+    return num + 1
+}
+
+
+// way 2 : in this method function is stored in a variable so we cannot access before the initialization. this method called mini hoisting
+
+addTwo(5)
+const addTwo = function(num){
+    return num + 2
+}
