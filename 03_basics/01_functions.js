20649963d05448c214ac048a2870865cc1711d3b
Dev Jivani <djjivanidjpatel@gmail.com>
2025-08-23 19:47:22 +0530

    object in function

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -38,3 +38,43 @@ console.log(loginUserMessage());
 
 // console.log(loginUserMessage("hitesh"))
 // console.log(loginUserMessage("hitesh"))
+
+
+
+//**********************************************************************
+
+
+
+function calculateCartPrice(val1, val2, ...num1){            // all the values are stored ...num1 and num1 is retured the array
+    return num1
+}
+
+// console.log(calculateCartPrice(200, 400, 500, 2000))
+
+const user = {
+    username: "hitesh",
+    price: 199
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+// handleObject(user)
+
+handleObject({
+    username: "sam",
+    price: 399
+})
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 500, 1000]));
+
+
+
