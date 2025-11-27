5965ef58fadaeb96d53919222083bcabbdbcd114
Dev Jivani <djjivanidjpatel@gmail.com>
2025-11-27 18:42:18 +0530

    update string

--- 01_basics/05_string.js
+++ 01_basics/05_string.js
@@ -8,29 +8,34 @@ const repoCount = 50
 const gameName = new String('hitesh-hc-com')
 
 
-console.log(gameName[0]);
+// console.log(gameName[0]);
 // console.log(gameName.__proto__);
 
 
-// // console.log(gameName.length);
-// // console.log(gameName.toUpperCase());
-console.log(gameName.charAt(2));
-// console.log(gameName.indexOf('t'));
+// console.log(gameName.length);  // 13
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));  // t
+// console.log(gameName.indexOf('h'));  // 0
 
-// const newString = gameName.substring(0, 4)
+const newString = gameName.substring(0, 4)
 // console.log(newString);   // hite
 
-const anotherString = gameName.slice(-2, 4)
-console.log(anotherString);
+const anotherString = gameName.slice(-6, -4)
+// console.log(anotherString); // hc
 
-// const newStringOne = "   hitesh    "
-// console.log(newStringOne);   // "   hitesh    "
-// console.log(newStringOne.trim());  // hitesh
+const newStringOne = "   hitesh    "
+console.log("newStringOne:"+newStringOne);   // "   hitesh    "
 
-// const url = "https://hitesh.com/hitesh%20choudhary"
+// trim function is used for remove the whitespace
+console.log(newStringOne.trim());  // hitesh
 
-// console.log(url.replace('%20', '-'))
+const url = "https://hitesh.com/hitesh%20choudhary"
 
-// console.log(url.includes('sundar'))   //  check word is available or not
+console.log(url.replace('%20', '-'))
 
-// console.log(gameName.split('-'));  
+console.log(url.includes('sundar'))   //  check word is available or not
+
+console.log(gameName.split('h'));  // [ '', 'ites', '-', 'c-com' ]
+
+console.log(gameName.split('-'));  // [ 'hitesh', 'hc', 'com' ]
+
