9b6c7b133bf9549ac26bd8e42e2d7ed21f2d0bb1
Dev Jivani <djjivanidjpatel@gmail.com>
2025-11-27 23:38:33 +0530

    update

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -1,36 +1,41 @@
 const score = 400
 // console.log(score);
 
-const balance = new Number(100)
-// console.log(balance);
+// const balance = new Number(100)
+// console.log(balance);  // [Number: 100]
+// console.log(typeof balance);  // object
 
-console.log(balance.toString().length);
-console.log(balance.toFixed(3));   // 100.0    ,   if 3 -> 100.000
+// const a = String(balance)
+// console.log(a.length); // 3
 
-const otherNumber = 123.8666
 
-console.log(typeof otherNumber)
+// console.log(balance.toString().length); // 3
+// console.log(balance.toFixed());   // 100.0    ,   if 3 -> 100.000
 
-console.log(otherNumber.toPrecision(2));  //  1.2e+2
+// const otherNumber = 123.8666
 
-const hundreds = 1000000
-console.log(hundreds.toLocaleString('en-IN'));  //  10,00,000
+// console.log(typeof otherNumber) // number
 
-// +++++++++++++ Maths +++++++++++++++++++++++++++++
+// console.log(otherNumber.toPrecision(2));  //  1.2e+2
 
-console.log(Math);
-console.log(Math.abs(-4));   // remove negative sign
-console.log(Math.round(4.6)); // 4
+// const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));  //  10,00,000
+
+// // +++++++++++++ Maths +++++++++++++++++++++++++++++
+
+console.log(Math); // here Math is object that provide the mathematical function and constants 
+console.log(Math.abs(-4));   // here abs means absolute and its remove negative sign 
+console.log(Math.round(4.4)); // 4
 console.log(Math.ceil(4.2));  // 5
 console.log(Math.floor(4.9));  // 4
 console.log(Math.min(4, 3, 6, 8));  // 3
 console.log(Math.max(4, 3, 6, 8));  // 8
 
-// console.log(Math.random());  // range between 0 to 1 any decimal value
-// console.log((Math.random()*10) + 1);
-// console.log(Math.floor(Math.random()*10) + 1);
+console.log(Math.random());  // range between 0 to 1 any decimal value
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
 
-// const min = 10
-// const max = 20
+const min = 10
+const max = 20
 
-// console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
