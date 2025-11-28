e1387dbb594adce04bc6bd1c08ea1f82777e9a5b
Dev Jivani <djjivanidjpatel@gmail.com>
2025-11-28 08:28:51 +0530

    updated

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -6,14 +6,22 @@ const myHeors = ["shaktiman", "naagraj"]
 const myArr2 = new Array(1, 2, 3, 4)
 // console.log(myArr[1]);
 
-// Array methods
+// console.log("Before Push :" + myArr);
+
+
+// // Array methods
 
 // myArr.push(6)
 // myArr.push(7)
-// myArr.pop()
+// myArr.pop()  // last value remove
+
+// console.log("Aefore Push :" + myArr);
 
 // myArr.unshift(9)     // 9 is added to the first index
-// myArr.shift()  // first index value is removed
+// // myArr.shift()  // first index value is removed
+
+// console.log(myArr);
+
 
 // console.log(myArr.includes(9));
 // console.log(myArr.indexOf(3));
@@ -21,19 +29,20 @@ const myArr2 = new Array(1, 2, 3, 4)
 // const newArr = myArr.join()       // joins convert the array values in string
 
 // console.log(myArr);
-// console.log( newArr);
+// console.log(typeof newArr);
 
 
 // slice, splice
 
-console.log("A ", myArr);   //   A  [ 0, 1, 2, 3, 4, 5 ]
+// console.log("A ", myArr);   //   A  [ 0, 1, 2, 3, 4, 5 ]
 
-const myn1 = myArr.slice(1, 3)   
+// const myn1 = myArr.slice(1, 3)   
 
-console.log(myn1);  // [ 1, 2 ]
+// console.log(myn1);  // [ 1, 2 ]
 console.log("B ", myArr);   //  B  [ 0, 1, 2, 3, 4, 5 ]
 
 
-const myn2 = myArr.splice(1, 3)
+const myn2 = myArr.splice(2, 3)
 console.log(myn2);   //  [ 1, 2, 3 ]
-console.log("C ", myArr);       //   C  [ 0, 4, 5 ]
+// console.log("C ", myArr);       //   C  [ 0, 4, 5 ]
+
